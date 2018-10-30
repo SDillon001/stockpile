bb279dbee89c9df8f70184f496a4feb85ccbe482
Steve Dillon <stevedillon001@gmail.com>
2018-10-29 20:40:04 -0700

    2

--- products.php
+++ products.php
@@ -2,15 +2,15 @@
 //Create the connection and select database
 include 'connection.php';
 
-error_reporting(E_ALL); ini_set('display_errors', 1);
+//error_reporting(E_ALL); ini_set('display_errors', 1);
 
 //Select all data from the Products table
 $sql = "SELECT * FROM Products ORDER BY id ASC";
 
 //Execute Query
 $result = mysqli_query($conn, $sql);
-
 //Did the query fail?
+
 if(!$result) die("Query has failed!");
 
 //Make result into PHP array
@@ -18,15 +18,15 @@ $Products = array();
 
 if(mysqli_num_rows($result)){
     while($Product = mysqli_fetch_assoc($result)){
-        $Products[] = array('Product'=>$Product);
+        $Products[] = $Product;
     }
 }
 
-//Output data in JSON format
-header('Content-type: application/json');
-echo json_encode(array('Products'=>$Products));
-
 //Close connection
 mysqli_close($conn);
+
+//Output data in JSON format
+header('Content-type: application/json');
+echo json_encode($Products);
 
 ?>
